ab4a26caa1a67d85f151e266342f9e706ded136b
Gwen McArdle <mcardleg@tcd.ie>
2022-11-27 00:39:42 +0000

    access keys removed

--- src/main/java/ie/tcd/mcardleg/CycLQE/Constants.java
+++ src/main/java/ie/tcd/mcardleg/CycLQE/Constants.java
@@ -1,6 +1,7 @@
 package ie.tcd.mcardleg.CycLQE;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class Constants {
     public static final String RAW_ACCELEROMETER_TABLE = "CycLQE_Raw_Accelerometer_Data_Table";
@@ -8,6 +9,7 @@ public class Constants {
     public static final String PROCESSED_DATA_TABLE = "CycLQE_Processed_Data_Table";
 
     public static final String UUID = "uuid";
+    public static final String USER_ID = "userId";
     public static final String TTL = "expiration";
     public static final String LATITUDE = "latitude";
     public static final String LONGITUDE = "longitude";
@@ -15,14 +17,12 @@ public class Constants {
     public static final String DATE_TIME = "dateTime";
 
 
-    public static final String AWS_ACCESS_KEY = "AKIAZNS5DHNSOONLMLT5";
-    public static final String AWS_SECRET_ACCESS_KEY = "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";
 
-    public static final BigDecimal LAT_INTERVAL = new BigDecimal("0.000005");
+    public static final BigDecimal LAT_INTERVAL = new BigDecimal("0.0005");
     public static final BigDecimal LON_INTERVAL = new BigDecimal("0.001");
     public static final BigDecimal DUBLIN_NORTHWEST_LAT = new BigDecimal("53.3494");
     public static final BigDecimal DUBLIN_NORTHWEST_LON = new BigDecimal("-6.32");
     public static final BigDecimal DUBLIN_SOUTHEAST_LAT = new BigDecimal("53.3414");
-    public static final BigDecimal DUBLIN_SOUTHEAST_LON = new BigDecimal("-6.26");
+    public static final BigDecimal DUBLIN_SOUTHEAST_LON = new BigDecimal("-6.25");
 
 }
